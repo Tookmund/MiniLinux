736f4bddee02e7605bd9be96fc693f5068b6308e
Jacob Adams <jacob@tookmund.com>
2019-06-10 17:47:41 -0400

    build.sh: Build everything

--- build.sh
+++ build.sh
@@ -5,4 +5,18 @@ cd musl-cross-make
 cp ../config.mak .
 make -j4
 make install
-cd ..
+
+cd ../busybusy
+make
+
+cd ../dropbear
+make
+
+cd ../musl-cross-make/linux-*/
+cp ../../.config .
+make oldconfig
+export PATH=$PWD/../../tools/bin:$PATH
+../../initramfs.sh
+make -j$(nproc)
+cp arch/x86/boot/bzImage ../../
+du -h ../../bzImage
